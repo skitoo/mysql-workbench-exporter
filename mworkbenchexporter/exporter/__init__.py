1890340a8bf247cfb9e4b827e801ec5f27ef5b35
skitoo <alexis.couronne@gmail.com>
2011-12-28 10:36:33 +0100

    add 'list' command add ENGINES_LIST in mworkbenchexporter.exporter package

--- mworkbenchexporter/exporter/__init__.py
+++ mworkbenchexporter/exporter/__init__.py
@@ -4,6 +4,7 @@ from mworkbenchexporter.exporter.doctrine2.annotation import Doctrine2Annotation
 
 DOCTRINE2_ANNOTATION = 'doctrine2:annotation'
 
+ENGINES_LIST = [DOCTRINE2_ANNOTATION]
 
 def get_exporter(engine):
     if engine == DOCTRINE2_ANNOTATION:
